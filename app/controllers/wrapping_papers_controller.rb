197bfff919229fdc707c81730cdc60fceb099d0e
Eve Reichmann <eve.reichmann@gmail.com>
2020-08-17 22:15:43 -0500

    CORS iniciated, controller functions

--- app/controllers/wrapping_papers_controller.rb
+++ app/controllers/wrapping_papers_controller.rb
@@ -1,2 +1,11 @@
 class WrappingPapersController < ApplicationController
+    def index
+        @wrapping_papers = WrappingPaper.all
+        render json: @wrapping_papers
+    end
+
+    def show 
+        @wrapping_paper = WrappingPaper.find(params[:id])
+        render json: @wrapping_paper
+    end
 end
