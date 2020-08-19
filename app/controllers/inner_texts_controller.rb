f2b08720ce7a706c5084a72e7c7ba2bacdc3c741
Eve Reichmann <eve.reichmann@gmail.com>
2020-08-19 12:56:10 -0500

    working in controllers

--- app/controllers/inner_texts_controller.rb
+++ app/controllers/inner_texts_controller.rb
@@ -2,5 +2,35 @@ class InnerTextsController < ApplicationController
     def show
         @inner_text = InnerText.find(params[:id])
         render json: @inner_text
+    end 
+
+    def create
+        @inner_text = InnerText.new(inner_text_params)
+        if @inner_text.save
+            render json: @inner_text
+        else
+            render json: @inner_text.errors
+        end        
+    end 
+
+    def update
+        @inner_text = InnerText.find(params[:id])
+        if @inner_text.update
+            render json: @inner_text
+        else
+            render json: @inner_text.errors
+        end        
+    end
+
+
+    def destroy
+        @inner_text= InnerText.find(params[:id])
+        @inner_text.destroy
     end    
+
+    private
+
+    def inner_text_params
+        params.require(:inner_text).permit(:inner_text, :wrapping_paper_id)
+    end
 end
--- app/controllers/wrapping_papers_controller.rb
+++ app/controllers/wrapping_papers_controller.rb
@@ -8,4 +8,34 @@ class WrappingPapersController < ApplicationController
         @wrapping_paper = WrappingPaper.find(params[:id])
         render json: @wrapping_paper
     end
+
+    def create
+        @wrapping_paper = WrappingPaper.new(wrapping_paper_params)
+        if @wrapping_paper.save
+            render json: @wrapping_paper
+        else
+            render json: @wrapping_paper.errors
+        end        
+    end 
+
+    def update
+        @wrapping_paper = WrappingPaper.find(params[:id])
+        if @wrapping_paper.update
+            render json: @wrapping_paper
+        else
+            render json: @wrapping_paper.errors
+        end        
+    end
+
+
+    def destroy
+        @wrapping_paper = WrappingPaper.find(params[:id])
+        @wrapping_paper.destroy
+    end    
+
+    private
+
+    def wrapping_paper_params
+        params.require(:wrapping_paper).permit(:name, :layout, :user_id)
+    end    
 end
