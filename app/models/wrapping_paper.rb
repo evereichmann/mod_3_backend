d6bca4d236a5d93f8f253d2d85cc32152dbb208a
Eve Reichmann <eve.reichmann@gmail.com>
2020-08-19 13:31:20 -0500

    edit controller

--- app/models/wrapping_paper.rb
+++ app/models/wrapping_paper.rb
@@ -4,5 +4,5 @@ class WrappingPaper < ApplicationRecord
     has_many :wrapping_paper_images
     has_many :images, through: :wrapping_paper_images
 
-    accepts_nested_attributes_for :user, :images, :inner_text
+    #accepts_nested_attributes_for :user, :images, :inner_text
 end
